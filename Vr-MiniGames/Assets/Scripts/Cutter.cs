a176ed6c2e24106dbed65b94cd6ce3db5387797d
Andrew Cardwell <andrewcardwell@btinternet.com>
2016-07-27 14:29:16 +0100

    Added Cutter Scripts and cube to test cutting

---
+++ Vr-MiniGames/Assets/Scripts/Cutter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Cutter : MonoBehaviour {
+
+	public Material capMaterial;
+
+    void OnCollisionEnter (Collision Collision)
+    {
+        GameObject victim = Collision.collider.gameObject;
+
+        GameObject[] pieces = MeshCutter.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+        if (!pieces[1].GetComponent<Rigidbody>())
+            pieces[1].AddComponent<Rigidbody>();
+
+       Destroy(pieces[1], 1);
+    }
+
+}
